7c27bb4610ad0e72c68ef641ee20407976ecc5cf
AndreaScuola <162697678+AndreaScuola@users.noreply.github.com>
2025-11-09 20:20:34 +0100

    Base classe Database.java con pattern Singleton

--- src/Database.java
+++ src/Database.java
@@ -1,3 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
+    private static Database instance;
+    private Connection connection;
+
+    private Database() {
+        try{
+            String url = "jdbc:sqlite:database/Spotify.db";
+            connection = DriverManager.getConnection(url);
+            System.out.println("Connected to database");
+        } catch (SQLException e) {
+            System.err.println("Errore connessione: " + e.getMessage());
+        }
+    }
+
+    public static Database getInstance() {
+        if (instance == null)
+            instance = new Database();
+
+        return instance;
+    }
+
 
 }
