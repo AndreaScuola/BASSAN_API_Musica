9fbf3fc0d9940410df48c52aca8df77caed484d6
AndreaScuola <162697678+AndreaScuola@users.noreply.github.com>
2025-11-09 22:01:24 +0100

    Base main (modificare menuDbLocale con nuovo output del menu + fare testing)

--- src/Database.java
+++ src/Database.java
@@ -1,6 +1,5 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class Database {
     private static Database instance;
@@ -23,5 +22,168 @@ public class Database {
         return instance;
     }
 
+    private boolean testConnection() {
+        try{
+            if(connection == null || !connection.isValid(5)) {
+                System.err.println("Errore di connessione al DB locale");
+                return false;
+            }
+        } catch (SQLException e) {
+            System.err.println("Errore di connessione al DB locale: " + e.getMessage());
+            return false;
+        }
 
+        return true;
+    }
+
+    public String selectAllArtisti(){
+        String result = "";
+        if(testConnection()) {
+            String query = "SELECT * FROM Artisti";
+            try{
+                PreparedStatement stmt = connection.prepareStatement(query);
+                ResultSet rs = stmt.executeQuery();
+                while(rs.next()){
+                    result = rs.getString(1) + "\t"
+                            + rs.getString(2) + "\t"
+                            + rs.getString(3) + "\t"
+                            + rs.getString(4) + "\n";
+                }
+            } catch (SQLException e) {
+                System.err.println("Errore di query: " + e.getMessage());
+                return null;
+            }
+
+            return result;
+        }
+
+        return null;
+    }
+
+    public String selectArtistaByID(int ID){
+        String result = "";
+        if(testConnection()) {
+            String query = "SELECT * FROM Artista WHERE ID = ?";
+
+            try{
+                PreparedStatement stmt = connection.prepareStatement(query);
+                stmt.setInt(1, ID);
+                ResultSet rs = stmt.executeQuery();
+
+                result = rs.getString(1) + "\t"
+                         + rs.getString(2) + "\t"
+                         + rs.getString(3) + "\t"
+                         + rs.getString(4) + "\n";
+
+                return result;
+            } catch (SQLException e) {
+                System.err.println("Errore di query: " + e.getMessage());
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    public String selectAllCanzoni() {
+        String result = "";
+        if(testConnection()) {
+            String query = "SELECT * FROM Canzoni";
+
+            try{
+                PreparedStatement stmt = connection.prepareStatement(query);
+                ResultSet rs = stmt.executeQuery();
+
+                result = rs.getString(1) + "\t"
+                        + rs.getString(2) + "\t"
+                        + rs.getString(3) + "\t"
+                        + rs.getString(4) + "\t"
+                        + rs.getString(5) + "\n";
+
+                return result;
+            } catch (SQLException e) {
+                System.err.println("Errore di query: " + e.getMessage());
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean createNewArtista(String nome, String paese, String genere) {
+        if(testConnection()) {
+            String query = "INSERT INTO Artisti (nome, paese, genere) VALUES (?, ?, ?)";
+            try {
+                PreparedStatement stmt = connection.prepareStatement(query);
+                stmt.setString(1, nome);
+                stmt.setString(2, paese);
+                stmt.setString(3, genere);
+                stmt.executeUpdate();
+
+                return true;
+            } catch (SQLException e) {
+                System.err.println("Errore di query: " + e.getMessage());
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean deleteArtistaByID(int ID) {
+        if(testConnection()) {
+            String query = "DELETE FROM Artisti WHERE ID = ?";
+            try {
+                PreparedStatement stmt = connection.prepareStatement(query);
+                stmt.setInt(1, ID);
+                stmt.executeUpdate();
+
+                return true;
+            } catch (SQLException e) {
+                System.err.println("Errore di query: " + e.getMessage());
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean createNewCanzone(String titolo, int durata, int annoPubblicazione, int IdArtista) {
+        if(testConnection()) {
+            String query = "INSERT INTO Artisti (titolo, durata, annoPublicazione, idArtista) VALUES (?, ?, ?, ?)";
+            try {
+                PreparedStatement stmt = connection.prepareStatement(query);
+                stmt.setString(1, titolo);
+                stmt.setInt(2, durata);
+                stmt.setInt(3, annoPubblicazione);
+                stmt.setInt(4, IdArtista);
+                stmt.executeUpdate();
+
+                return true;
+            } catch (SQLException e) {
+                System.err.println("Errore di query: " + e.getMessage());
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean deleteCanzoneByID(int ID) {
+        if(testConnection()) {
+            String query = "DELETE FROM Canzoni WHERE ID = ?";
+            try {
+                PreparedStatement stmt = connection.prepareStatement(query);
+                stmt.setInt(1, ID);
+                stmt.executeUpdate();
+
+                return true;
+            } catch (SQLException e) {
+                System.err.println("Errore di query: " + e.getMessage());
+                return false;
+            }
+        }
+
+        return false;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     static API api = new API();
+    static Database db = Database.getInstance();
 
     public static void main(String[] args) {
         int scelta;
@@ -159,38 +160,21 @@ public class Main {
 
             switch (scelta){
                 case 1:
-                    ArrayList<EntitaArtista> artisti = api.selectAllArtisti();
-                    for(EntitaArtista artista : artisti)
-                        System.out.println(artista.toString());
+                    System.out.println(db.selectAllArtisti());
                     break;
+
                 case 2:
                     System.out.print("Inserisci ID dell'artista: ");
                     int ID = sc.nextInt();
                     sc.nextLine();
-                    EntitaArtista artista = api.selectArtistaByID(ID);
-                    System.out.println(artista.toString());
+                    System.out.println(db.selectArtistaByID(ID));
                     break;
+
                 case 3:
-                    System.out.print("Inserisci ID dell'artista: ");
-                    ID = sc.nextInt();
-                    sc.nextLine();
-                    ArrayList<EntitaCanzone> canzoni = api.selectCanzoniByArtistaID(ID);
-                    for(EntitaCanzone c : canzoni)
-                        System.out.println(c.toString());
+                    System.out.println(db.selectAllCanzoni());
                     break;
+
                 case 4:
-                    canzoni = api.selectAllCanzoni();
-                    for(EntitaCanzone c : canzoni)
-                        System.out.println(c.toString());
-                    break;
-                case 5:
-                    System.out.print("Inserisci ID della canzone: ");
-                    ID = sc.nextInt();
-                    sc.nextLine();
-                    EntitaCanzone canzone = api.selectCanzoneByID(ID);
-                    System.out.println(canzone.toString());
-                    break;
-                case 6:
                     System.out.print("Inserisci nome artista: ");
                     String nome = sc.nextLine();
                     System.out.print("Inserisci paese: ");
@@ -198,37 +182,55 @@ public class Main {
                     System.out.print("Inserisci genere: ");
                     String genere = sc.nextLine();
 
-                    String nuovoArtistaJson = "{\"nome\":\"" + nome + "\",\"paese\":\"" + paese + "\",\"genere\":\"" + genere + "\"}";
-                    EntitaArtista rispostaPost = api.postNewArtista(nuovoArtistaJson);
-                    System.out.println("Artista creato: " + rispostaPost);
+                    if(db.createNewArtista(nome, paese, genere))
+                        System.out.println("Artista creato con successo");
+                    else
+                        System.out.println("Errore nella creazione dell'artista");
                     break;
 
-                case 7:
-                    System.out.print("Inserisci ID artista da aggiornare: ");
-                    ID = sc.nextInt();
-                    sc.nextLine();
-                    System.out.print("Nuovo nome artista: ");
-                    nome = sc.nextLine();
-                    System.out.print("Nuovo paese: ");
-                    paese = sc.nextLine();
-                    System.out.print("Nuovo genere: ");
-                    genere = sc.nextLine();
-
-                    nuovoArtistaJson = "{\"nome\":\"" + nome + "\",\"paese\":\"" + paese + "\",\"genere\":\"" + genere + "\"}";
-                    EntitaArtista rispostaPut = api.putNewArtista(ID, nuovoArtistaJson);
-                    System.out.println("Artista aggiornato: " + rispostaPut.toString());
-                    break;
-
-                case 8:
+                case 5:
                     System.out.print("Inserisci ID artista da eliminare: ");
                     ID = sc.nextInt();
                     sc.nextLine();
 
-                    EntitaArtista rispostaDelete = api.deleteArtistaByID(ID);
-                    System.out.println("Risposta delete: " + rispostaDelete);
+                    if(db.deleteArtistaByID(ID))
+                        System.out.println("Artista eliminato con successo");
+                    else
+                        System.out.println("Errore nell'eliminazione dell'artista");
+                    break;
+
+                case 6:
+                    System.out.print("Inserisci titolo canzone: ");
+                    String titolo = sc.nextLine();
+                    System.out.print("Inserisci durata canzone: ");
+                    int durata = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Inserisci anno pubblicazione canzone: ");
+                    int annoPublicazione = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Inserisci id dell'artista che ha creato la canzone: ");
+                    int idArtista = sc.nextInt();
+                    sc.nextLine();
+
+                    if(db.createNewCanzone(titolo, durata, annoPublicazione, idArtista))
+                        System.out.println("Canzone creata con successo");
+                    else
+                        System.out.println("Errore nella creazione della canzone");
+
+                    break;
+
+                case 7:
+                    System.out.print("Inserisci ID canzone da eliminare: ");
+                    ID = sc.nextInt();
+                    sc.nextLine();
+
+                    if(db.deleteCanzoneByID(ID))
+                        System.out.println("Canzone eliminata con successo");
+                    else
+                        System.out.println("Errore nell'eliminazione della canzone");
                     break;
             }
 
-        }while(scelta >= 1 && scelta <= 8);
+        }while(scelta >= 1 && scelta <= 7);
     }
 }
